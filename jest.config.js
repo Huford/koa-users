45f860b1eb37aff47b0043569d08dab4d09d55e2
Julian <julito.ar@gmail.com>
2021-04-20 23:45:02 -0300

    add coveralls

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-babel',
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.{,ts}'],
+  coverageReporters: ['json-summary', 'lcov'],
 };
